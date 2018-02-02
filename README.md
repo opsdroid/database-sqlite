a0fe50a4472b08d7aded0f203e22ab8f21999612
Angel Fernandez <angelfernandezibanez@gmail.com>
2018-02-02 18:25:15 +0100

    implement the sqlite interface

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# database-odbc
-💾 An odbc database module for opsdroid 
+opsdroid database sqlite
+========================
+
+A database module for [opsdroid](https://github.com/opsdroid/opsdroid) to persist memory in a sqlite database.
+
+
+## Requirements
+
+None.
+
+## Configuration
+
+```yaml
+databases:
+  sqlite:
+    file: "my_file.db"  # (optional) default "sqlite.db"
+    table: "my_table"  # (optional) default "opsdroid"
+```
---
+++ sqlite.py
@@ -0,0 +1,55 @@
+import logging
+import json
+import sqlite3
+
+from opsdroid.database import Database
+
+
+class DatabaseSqlite(Database):
+    """A module for opsdroid to allow persist in a sqlite database."""
+
+    def __init__(self, config):
+        """Start the database connection."""
+        logging.debug("Loaded sqlite database connector")
+        self.name = "sqlite"
+        self.config = config
+        self.conn = None
+        self.table = None
+
+    async def connect(self, opsdroid):
+        """Connect to the database."""
+        file = self.config.get("file", "sqlite.db")
+        table = self.config.get("table", "opsdroid")
+
+        self.conn = sqlite3.connect(file, isolation_level=None)
+        self.table = table
+        self.conn.execute(
+            "CREATE TABLE IF NOT EXISTS {} (key text PRIMARY KEY, data text)"
+            .format(self.table)
+        )
+        logging.info("Connected to sqlite {}".format(file))
+
+    async def put(self, key, data):
+        """Insert or replace an object into the database for a given key."""
+        logging.debug("Putting " + key + " into sqlite")
+        json_data = json.dumps(data)
+        cur = self.conn.cursor()
+        cur.execute(
+            "DELETE FROM {} WHERE key=?".format(self.table), (key,))
+        cur.execute(
+            "INSERT INTO {} VALUES (?, ?)".format(self.table),
+            (key, json_data))
+        cur.close()
+
+    async def get(self, key):
+        """Get data from the database for a given key."""
+        logging.debug("Getting " + key + " from sqlite")
+        data = None
+        cur = self.conn.cursor()
+        cur.execute(
+            "SELECT data FROM {} WHERE key=?".format(self.table), (key,))
+        row = cur.fetchone()
+        if row:
+            data = json.loads(row[0])
+        cur.close()
+        return data
