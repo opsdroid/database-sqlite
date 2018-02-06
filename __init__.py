1a8909897c80cb027c577c3b893a0a73e48747b3
Angel Fernandez <angelfernandezibanez@gmail.com>
2018-02-06 13:03:45 +0100

    use aiosqlite instead of sqlite3 (fixes #2)

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import logging
 import json
-import sqlite3
+import aiosqlite
 
 from opsdroid.database import Database
 
@@ -13,43 +13,44 @@ class DatabaseSqlite(Database):
         logging.debug("Loaded sqlite database connector")
         self.name = "sqlite"
         self.config = config
-        self.conn = None
+        self.conn_args = {'isolation_level': None}
+        self.db_file = None
         self.table = None
 
     async def connect(self, opsdroid):
         """Connect to the database."""
-        db_file = self.config.get("file", "sqlite.db")
-        table = self.config.get("table", "opsdroid")
+        self.db_file = self.config.get("file", "sqlite.db")
+        self.table = self.config.get("table", "opsdroid")
 
-        self.conn = sqlite3.connect(db_file, isolation_level=None)
-        self.table = table
-        self.conn.execute(
-            "CREATE TABLE IF NOT EXISTS {} (key text PRIMARY KEY, data text)"
-            .format(self.table)
-        )
-        logging.info("Connected to sqlite {}".format(db_file))
+        async with aiosqlite.connect(self.db_file, **self.conn_args) as db:
+            await db.execute(
+                "CREATE TABLE IF NOT EXISTS {}"
+                "(key text PRIMARY KEY, data text)"
+                .format(self.table)
+            )
+        logging.info("Connected to sqlite {}".format(self.db_file))
 
     async def put(self, key, data):
         """Insert or replace an object into the database for a given key."""
         logging.debug("Putting " + key + " into sqlite")
         json_data = json.dumps(data)
-        cur = self.conn.cursor()
-        cur.execute(
-            "DELETE FROM {} WHERE key=?".format(self.table), (key,))
-        cur.execute(
-            "INSERT INTO {} VALUES (?, ?)".format(self.table),
-            (key, json_data))
-        cur.close()
+        async with aiosqlite.connect(self.db_file, **self.conn_args) as db:
+            cur = await db.cursor()
+            await cur.execute(
+                "DELETE FROM {} WHERE key=?".format(self.table), (key,))
+            await cur.execute(
+                "INSERT INTO {} VALUES (?, ?)".format(self.table),
+                (key, json_data))
 
     async def get(self, key):
         """Get data from the database for a given key."""
         logging.debug("Getting " + key + " from sqlite")
         data = None
-        cur = self.conn.cursor()
-        cur.execute(
-            "SELECT data FROM {} WHERE key=?".format(self.table), (key,))
-        row = cur.fetchone()
-        if row:
-            data = json.loads(row[0])
-        cur.close()
+        async with aiosqlite.connect(self.db_file, **self.conn_args) as db:
+            cur = await db.cursor()
+            await cur.execute(
+                "SELECT data FROM {} WHERE key=?".format(self.table), (key,))
+            row = await cur.fetchone()
+            if row:
+                data = json.loads(row[0])
         return data
